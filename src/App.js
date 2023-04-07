326b1d1f0538d4feaf9ba3dfcb4093023cd20976
VictorS215 <113443450+VictorS215@users.noreply.github.com>
2023-04-07 14:22:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function InputOutput() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label htmlFor="input1">Input 1:</label>
+        <input
+          id="input1"
+          type="text"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="input2">Input 2:</label>
+        <input
+          id="input2"
+          type="text"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="input3">Input 3:</label>
+        <input
+          id="input3"
+          type="text"
+          value={input3}
+          onChange={(e) => setInput3(e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Output 1:</h3>
+        <div>{input1}</div>
+      </div>
+      <div>
+        <h3>Output 2:</h3>
+        <div>{input2}</div>
+      </div>
+      <div>
+        <h3>Output 3:</h3>
+        <div>{input3}</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default InputOutput;
